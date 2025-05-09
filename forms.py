ec5c29a6d81d26ba3fee52d20a84537905f70640
Sandeepsng61 <88820860+Sandeepsng61@users.noreply.github.com>
2025-05-09 22:42:22 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -10,46 +10,82 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
+class ForgotPasswordForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New Password', 
+                             validators=[DataRequired(), 
+                                        Length(min=6)])
+    confirm_password = PasswordField(
+        'Confirm New Password', 
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                                       Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         Length(min=6)])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[DataRequired(),
+                                        EqualTo('password')])
     submit = SubmitField('Register')
 
 
 class ProfileForm(FlaskForm):
+    # Personal Information
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Phone Number', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    state = StringField('State', validators=[DataRequired()])
-    country = StringField('Country', validators=[DataRequired()])
-    zipcode = StringField('ZIP Code', validators=[DataRequired()])
+    phone = StringField('Mobile Number (10 digits)', validators=[DataRequired()])
+    
+    # Address Information
+    country = SelectField('Country', validators=[DataRequired()], choices=[('india', 'India')])
+    state = SelectField('State', validators=[DataRequired()], choices=[
+        ('', 'Select State'),
+        ('andhra_pradesh', 'Andhra Pradesh'),
+        ('telangana', 'Telangana'),
+        ('karnataka', 'Karnataka'),
+        ('tamil_nadu', 'Tamil Nadu'),
+        ('maharashtra', 'Maharashtra'),
+        ('delhi', 'Delhi'),
+        ('uttar_pradesh', 'Uttar Pradesh')
+    ])
+    city = StringField('City/District/Town', validators=[DataRequired()])
+    zipcode = StringField('Pin Code', validators=[DataRequired()])
+    locality = StringField('Locality', validators=[DataRequired()])
+    address = TextAreaField('Address (Area and Street)', validators=[DataRequired()])
+    
     submit = SubmitField('Update Profile')
 
 
 class ProductForm(FlaskForm):
     name = StringField('Product Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    price = FloatField('Price', validators=[DataRequired(), NumberRange(min=0)])
-    stock = IntegerField('Stock', validators=[DataRequired(), NumberRange(min=0)])
-    category = SelectField('Category', choices=[
-        ('cpu', 'CPU'),
-        ('gpu', 'GPU'),
-        ('motherboard', 'Motherboard'),
-        ('ram', 'RAM'),
-        ('storage', 'Storage'),
-        ('psu', 'Power Supply'),
-        ('case', 'Cabinet'),
-        ('cooling', 'Cooling'),
-        ('peripherals', 'Peripherals'),
-        ('monitor', 'Monitor'),
-        ('prebuilt_intel', 'Prebuilt PC - Intel'),
-        ('prebuilt_amd', 'Prebuilt PC - AMD')
-    ])
+    price = FloatField('Price',
+                       validators=[DataRequired(),
+                                   NumberRange(min=0)])
+    stock = IntegerField('Stock',
+                         validators=[DataRequired(),
+                                     NumberRange(min=0)])
+    category = SelectField('Category',
+                           choices=[('cpu', 'CPU'), ('gpu', 'GPU'),
+                                    ('motherboard', 'Motherboard'),
+                                    ('ram', 'RAM'), ('storage', 'Storage'),
+                                    ('psu', 'Power Supply'),
+                                    ('case', 'Cabinet'),
+                                    ('cooling', 'Cooling'),
+                                    ('peripherals', 'Peripherals'),
+                                    ('monitor', 'Monitor'),
+                                    ('prebuilt_intel', 'Prebuilt PC - Intel'),
+                                    ('prebuilt_amd', 'Prebuilt PC - AMD')])
     image_url = StringField('Image URL', validators=[DataRequired()])
     featured = BooleanField('Featured Product')
     specs = TextAreaField('Specifications (JSON)', validators=[Optional()])
@@ -57,28 +93,59 @@ class ProductForm(FlaskForm):
 
 
 class CheckoutForm(FlaskForm):
+    # Personal Information
     full_name = StringField('Full Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Phone Number', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    state = StringField('State', validators=[DataRequired()])
-    country = StringField('Country', validators=[DataRequired()])
-    zipcode = StringField('ZIP Code', validators=[DataRequired()])
-    payment_method = RadioField('Payment Method', choices=[
-        ('credit_card', 'Credit Card'),
-        ('debit_card', 'Debit Card'),
-        ('net_banking', 'Net Banking'),
-        ('upi', 'UPI'),
-        ('cod', 'Cash on Delivery')
-    ], validators=[DataRequired()])
-    submit = SubmitField('Continue to Payment')
+    phone = StringField('Mobile Number (10 digits)', validators=[DataRequired()])
+    alt_phone = StringField('Alternate Phone (Optional)')
+    
+    # Address Information
+    country = SelectField('Country', validators=[DataRequired()], choices=[('india', 'India')])
+    state = SelectField('State', validators=[DataRequired()], choices=[
+        ('', 'Select State'),
+        ('andhra_pradesh', 'Andhra Pradesh'),
+        ('telangana', 'Telangana'),
+        ('karnataka', 'Karnataka'),
+        ('tamil_nadu', 'Tamil Nadu'),
+        ('maharashtra', 'Maharashtra'),
+        ('delhi', 'Delhi'),
+        ('uttar_pradesh', 'Uttar Pradesh')
+    ])
+    city = StringField('City/District/Town', validators=[DataRequired()])
+    zipcode = StringField('Pin Code', validators=[DataRequired()])
+    locality = StringField('Locality', validators=[DataRequired()])
+    address = TextAreaField('Address (Area and Street)', validators=[DataRequired()])
+    landmark = StringField('Landmark (Optional)')
+    
+    # Address Type
+    address_type = RadioField('Address Type', 
+                             choices=[('home', 'Home (All day delivery)'),
+                                     ('work', 'Work (Delivery between 10 AM - 5 PM)')],
+                             validators=[DataRequired()],
+                             default='home')
+    
+    # Payment Method
+    payment_method = RadioField('Payment Method',
+                                choices=[('credit_card', 'Credit Card'),
+                                         ('debit_card', 'Debit Card'),
+                                         ('net_banking', 'Net Banking'),
+                                         ('upi', 'UPI'),
+                                         ('cod', 'Cash on Delivery')],
+                                validators=[DataRequired()])
+    
+    submit = SubmitField('Save and Deliver Here')
 
 
 class PaymentForm(FlaskForm):
     card_number = StringField('Card Number', validators=[Optional()])
     card_holder = StringField('Card Holder Name', validators=[Optional()])
-    expiration_month = SelectField('Month', choices=[(str(i), str(i).zfill(2)) for i in range(1, 13)], validators=[Optional()])
-    expiration_year = SelectField('Year', choices=[(str(i), str(i)) for i in range(2023, 2034)], validators=[Optional()])
+    expiration_month = SelectField('Month',
+                                   choices=[(str(i), str(i).zfill(2))
+                                            for i in range(1, 13)],
+                                   validators=[Optional()])
+    expiration_year = SelectField('Year',
+                                  choices=[(str(i), str(i))
+                                           for i in range(2023, 2034)],
+                                  validators=[Optional()])
     cvv = StringField('CVV', validators=[Optional()])
     submit = SubmitField('Pay Now')
